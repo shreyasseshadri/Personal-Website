c602107b07f6ad893bd1eaabe783b67c997e8f8b
shreyas <shreyasseshadri@gmail.com>
2018-07-24 15:37:05 +0530

    Added media

--- about/views.py
+++ about/views.py
@@ -3,13 +3,28 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.shortcuts import redirect
 # Create your views here.
+
 def pdf_view(request):
-    with open('/home/shreyas/Downloads/Resume.pdf', 'r') as pdf:
-        response = HttpResponse(pdf.read(), content_type='application/pdf')
-        response['Content-Disposition'] = 'inline;filename=some_file.pdf'
-        return response
-    pdf.closed
+    return redirect('/media/Resume.pdf')
+
+    # with open('/media/Resume.pdf', 'r') as pdf:
+    #     response = HttpResponse(pdf.read(), content_type='application/pdf')
+    #     response['Content-Disposition'] = 'inline;filename=some_file.pdf'
+    #     return response
+    # pdf.closed
+
+# from django.http import FileResponse, Http404
+#
+# def pdf_view(request):
+#     try:
+#         return FileResponse(open('Resume.pdf', 'rb'), content_type='application/pdf')
+#     except:
+#         # print("hello")
+#         raise Http404()
+
 def index(request):
     return render(request,'about/index.html',{})
+# /home/shreyas/personal_website/about/templates/about/Resume.pdf
+# /home/shreyas/personal_website/about/views.py
--- personal_website/urls.py
+++ personal_website/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 # from projects import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^about/',include('about.urls')),
     url(r'^projects/',include('projects.urls')),
     url(r'^',include('home.urls')),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
